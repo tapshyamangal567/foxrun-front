5b895254bddd6cdcaabfdd1e949fc55c9ec4cbf3
--get <kratikarajawat05@gmail.com>
2026-08-11 13:45:49 +0530

    Initial commit of Foxrun front website

---
+++ terms-of-service.php
@@ -0,0 +1,125 @@
+<?php include('header.php') ?>
+<main class="main">
+    <div class="inner-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div>
+                        <h4>Terms and Conditions</h4>
+                        <p>
+                            This Privacy Policy outlines how we collect, use, disclose, and protect your personal
+                            information when you visit our website. By accessing or using our website, you agree to the
+                            terms of this Privacy Policy and consent to the collection and use of your information as
+                            described herein. We are committed to ensuring that your privacy is protected. Should we ask
+                            you to provide certain information by which you can be identified when using this website,
+                            you can be assured that it will only be used in accordance with this Privacy Policy. We
+                            regularly review our compliance with this policy and ensure that all data handling practices
+                            are transparent and secure.
+                        </p>
+                    </div>
+                    <br>
+                    <div>
+                        <h4>Information We Collect</h4>
+                        <p>
+                            We collect personal information such as names, email addresses, and browsing data to enhance
+                            user experience and provide personalized services. This data helps us understand user
+                            preferences and improve our offerings. Your privacy is important to us, and we ensure that
+                            all information is handled with strict confidentiality.
+                        </p>
+                        <ul style="margin-left:30px; list-style:circle;">
+                            <li style="margin-bottom:10px;">
+                                <span><strong>Personal Information:</strong></span>
+                                <span>Name, email address, phone number, and other contact details.</span>
+                            </li>
+                            <li style="margin-bottom:10px;">
+                                <span><strong>Usage Data:</strong></span>
+                                <span>Information about how you use our website, including your IP address, browser
+                                    type, and pages visited.</span>
+                            </li>
+                            <li style="margin-bottom:10px;">
+                                <span><strong>Cookies and Tracking technology:</strong></span>
+                                <span>We use cookies to enhance your experience on our website. You can manage your
+                                    cookie preferences through your browser settings.</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <br>
+                    <div>
+                        <h4>How We Use Your Information</h4>
+                        <p>
+                            We use your information to provide and improve our services, ensuring a personalized
+                            experience tailored to your needs. This includes processing transactions, communicating
+                            updates, and responding to inquiries. Additionally, we use your data for analytical purposes
+                            to enhance our offerings and for security measures to protect against fraud.
+                        </p>
+                        <ul style="margin-left:30px; list-style:circle;">
+                            <li style="margin-bottom:10px;">To provide and maintain our services.</li>
+                            <li style="margin-bottom:10px;">To improve and personalize your experience on our website.
+                            </li>
+                            <li style="margin-bottom:10px;">To communicate with you, including sending updates and
+                                promotional materials.</li>
+                            <li style="margin-bottom:10px;">To analyze website usage and improve our services.</li>
+                        </ul>
+                    </div>
+                    <br>
+                    <div>
+                        <h4>Sharing Your Information</h4>
+                        <p>
+                            We do not sell, trade, or otherwise transfer your personal information to outside parties
+                            except as described in this Privacy Policy. We take reasonable steps to ensure that any
+                            third parties with whom we share your personal information are bound by appropriate
+                            confidentiality and security obligations regarding your personal information.
+                        </p>
+                        <p>
+                            We understand the importance of maintaining the privacy and security of your personal
+                            information. Therefore, we implement stringent measures to protect your data from
+                            unauthorized access, use, or disclosure. Our commitment to safeguarding your privacy
+                            includes:
+                        </p>
+                        <ul style="margin-left:30px; list-style:circle;">
+                            <li style="margin-bottom:10px;">
+                                <strong>Data Encryption:</strong>
+                                <span>We use advanced encryption technologies to protect your personal information
+                                    during transmission and storage. This ensures that your data is secure and
+                                    inaccessible to unauthorized parties.</span>
+                            </li>
+                            <li style="margin-bottom:10px;">
+                                <strong>Access Controls:</strong>
+                                <span>We restrict access to your personal information to only those employees,
+                                    contractors, and agents who need to know that information to process it on our
+                                    behalf. These individuals are subject to strict confidentiality obligations and may
+                                    be disciplined or terminated if they fail to meet these obligations.</span>
+                            </li>
+                            <li style="margin-bottom:10px;">
+                                <strong>Regular Audits:</strong>
+                                <span>We conduct regular audits of our data handling practices and security measures to
+                                    ensure compliance with this Privacy Policy and applicable laws. This helps us
+                                    identify and address any potential vulnerabilities in our systems.</span>
+                            </li>
+                            <li style="margin-bottom:10px;">
+                                <strong>Incident Response:</strong>
+                                <span>In the unlikely event of a data breach, we have established procedures to respond
+                                    promptly and effectively. We will notify you and any relevant authorities as
+                                    required by law and take all necessary steps to mitigate the impact of the
+                                    breach.</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <br>
+                    <div>
+                        <h4>Contact Us</h4>
+                        <p>
+                            If you have any questions about our privacy policy, please contact <a
+                                href="/ovoride/demo/contact"><strong>with us</strong></a>. Our team is available to
+                            assist you with any inquiries or concerns you may have regarding our privacy policy. We
+                            value your privacy and are committed to ensuring that your experience on our website is
+                            transparent and satisfactory.
+                        </p>
+                    </div>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include('footer.php') ?>
